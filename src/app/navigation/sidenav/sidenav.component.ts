20b3485f1fc7a367e61d4d11ea71609885248e12
tomkotlar <t.kotlar@yahoo.com>
2020-05-28 14:31:37 +0100

    Navigation/sidenav -material sidenav menu icons

--- src/app/navigation/sidenav/sidenav.component.ts
+++ src/app/navigation/sidenav/sidenav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-sidenav',
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidenav.component.scss']
 })
 export class SidenavComponent implements OnInit {
+  @Output() closeSideNav = new EventEmitter
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onClose() {
+    this.closeSideNav.emit()
   }
 
 }
