14d398a5e7f70b628630157cf1be8d51a67e9fa3
tomkotlar <t.kotlar@yahoo.com>
2020-05-28 10:47:51 +0100

    updating modules -mateial, flex-layout, components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//stylyng 
+import { MaterialModule } from './materials/material.module';
+import { FlexLayoutModule } from "@angular/flex-layout";
+//Components
+import { ToolbarComponent } from './navigation/toolbar/toolbar.component';
+import { SidenavComponent } from './navigation/sidenav/sidenav.component';
+import { PlanListComponent } from './plans/plan-list/plan-list.component';
+import { PlanDetailsComponent } from './plans/plan-details/plan-details.component';
+import { CartComponent } from './cart/cart.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    SidenavComponent,
+    PlanListComponent,
+    PlanDetailsComponent,
+    CartComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
