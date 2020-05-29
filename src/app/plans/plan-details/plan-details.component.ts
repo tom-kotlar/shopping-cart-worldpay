03b1a8d978e370bb323f5bc633e3be519bab05a3
tomkotlar <t.kotlar@yahoo.com>
2020-05-29 09:18:04 +0100

    Plans/Plan-details -detail view of the choose plan

--- src/app/plans/plan-details/plan-details.component.ts
+++ src/app/plans/plan-details/plan-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PlanService } from '../plan.service';
+import { MatBottomSheet, MatBottomSheetRef } from '@angular/material/bottom-sheet';
+import { planList } from 'src/assets/plan-list';
 
 @Component({
   selector: 'app-plan-details',
@@ -6,10 +10,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./plan-details.component.scss']
 })
 export class PlanDetailsComponent implements OnInit {
+  plan
+  constructor(private route: ActivatedRoute, private planService: PlanService, private _bottomSheet: MatBottomSheet) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      console.log(params, "index of planId")
+      this.plan = planList[+params.get('planId')]
+      console.log(this.plan)
+    })
+  }
+  share() {
+    window.alert('Share your Plan on social Media');
+  }
+  addToCart(plan) {
+    this.planService.addToCart(plan)
+    window.alert('Your plan has been added to the cart!');
   }
 
+  openBottomSheet(): void {
+    this._bottomSheet.open(BottomSheet);
+  }
+}
+
+@Component({
+  selector: 'bottom-sheet',
+  templateUrl: 'bottom-sheet.html',
+})
+export class BottomSheet {
+  constructor(private _bottomSheetRef: MatBottomSheetRef<BottomSheet>) {}
+
+  openLink(event: MouseEvent): void {
+    this._bottomSheetRef.dismiss();
+    event.preventDefault();
+  }
 }
