284b339f2cffc4af24124c62670c8fb3c74bb38c
tomkotlar <t.kotlar@yahoo.com>
2020-05-29 09:38:47 +0100

    Updating modules -routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PlanListComponent } from './plans/plan-list/plan-list.component';
 import { PlanDetailsComponent } from './plans/plan-details/plan-details.component';
+import { CartComponent } from './cart/cart.component';
 
 
 const routes: Routes = [
   {path: '', component: PlanListComponent},
   {path: 'planList/:planId', component: PlanDetailsComponent},
+  { path: 'cart', component: CartComponent },
 ];
 
 @NgModule({
