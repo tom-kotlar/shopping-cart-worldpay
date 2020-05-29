ad8e344644b9cd5959fdb3ce8024299c5a1eff8d
tomkotlar <t.kotlar@yahoo.com>
2020-05-29 09:13:26 +0100

    Plans/Pan-list -list of energy plan cards

--- src/app/plans/plan-list/plan-list.component.ts
+++ src/app/plans/plan-list/plan-list.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { planList } from "src/assets/plan-list";
+
+import {
+  MatSnackBar, MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
+
+
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-plan-list',
@@ -7,9 +16,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlanListComponent implements OnInit {
 
-  constructor() { }
+  planList = planList
+  durationInSeconds = 2;
+  horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+  verticalPosition: MatSnackBarVerticalPosition = 'bottom';
+
+  constructor(private _snackBar: MatSnackBar, public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
 
+  call() {
+    window.alert('You callling the customer service!');
+  }
+
+  openSnackBar() {
+    this._snackBar.openFromComponent(ChatComponent, {
+      duration: this.durationInSeconds * 1000,
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+    });
+  }
+
+  openDialog() {
+    this.dialog.open(ShareComponent);
+  }
+
 }
+
+
+@Component({
+  selector: 'app-plan-list',
+  templateUrl: './dialog-element.html',
+})
+export class ShareComponent { }
+
+@Component({
+  selector: 'app-plan-list',
+  templateUrl: './chat-element.html',
+})
+export class ChatComponent { }
